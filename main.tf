2cb51156026af7e5207ad423a7517730fc32ef5a
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2022-01-10 13:42:32 +0100

    less

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ terraform {
   required_version = ">= 1.1.2"
 
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.5.0"
-    }
-
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "2.7.1"
@@ -23,21 +18,10 @@ resource "kubernetes_stateful_set" "test" {
   spec {
 
     template {
-      metadata {
-        labels = {
-          app = "test"
-        }
-      }
-
       spec {
-        restart_policy                   = "Always"
-        termination_grace_period_seconds = 60
-
         container {
           name              = "test"
           image             = "busybox:latest"
-          image_pull_policy = "IfNotPresent"
-          command           = ["chown", "-R", "65534:65534", "/data"]
         }
       }
     }
